35ea3af2cf98e15ac0025c845e78730af3910d8b
Justin Kamal <kamaljustin@yahoo.com>
2019-07-25 14:56:13 -0400

    added glove

---
+++ glove.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+import numpy as np
+
+import gensim
+from gensim.models.keyedvectors import KeyedVectors
+
+path = r"C:\Users\Leviathan\Downloads\glove.6B.50d.txt.w2v\glove.6B.50d.txt.w2v"
+glove = KeyedVectors.load_word2vec_format(path, binary=False)
+
+with open("idfs.pickle", mode="rb") as opened_file:
+    word_idfs_file = pickle.load(opened_file)
+
+def glover(text):
+    vectors = np.zeros((50,))
+    text = text.split()
+    for word in text:
+
+        if word not in glove:
+            pass
+        else:
+            word_vector = glove[word]
+
+            word_idf = word_idfs_file[word]
+
+            vector = word_vector*word_idf
+
+            vectors += vector
+    return vectors
