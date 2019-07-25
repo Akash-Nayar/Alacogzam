1effc2e0535ea42cd1735069704baa535d5d9475
Akash Nayar <burgercheetah5211@gmail.com>
2019-07-25 16:35:06 -0400

    Generates training data

--- trainingdata.py
+++ trainingdata.py
@@ -2,12 +2,18 @@ import numpy as np
 import random
 import secrets
 
-def generate_training(n):
+def generate_training(captions, image_embeds, n):
 	"""
 	Generates triples to train the RNN
 
 	Parameters:
 	-----------
+	captions : dict
+		image_id : List[captions for that image]
+
+	imaged_embeds : dict
+		image_id : np.array(Embed for that image)
+		
 	n : int
 		The number of triples to create.
 
@@ -15,19 +21,19 @@ def generate_training(n):
 		list : n number of triples.
 	"""
 
-	#REPLACE THIS
 	triples = []
 
 	for i in range(n):
 		good_key = random.choice(dictionary.keys())
 
 		good_image = image_embeds[good_image]
-		good_caption = secrets.choice(dictionary[good_key])
-		index = random.choice(dictionary.keys())
+		good_caption = secrets.choice(captions[good_key])
+		index = random.choice(captions.keys())
 		while index != good_key:
-			index = random.choice(dictionary.keys())
+			index = random.choice(captions.keys())
 		bad_image = image_embeds[index]
 		triples.append(good_caption, good_image, bad_image)
+
 	return triples
 
 
